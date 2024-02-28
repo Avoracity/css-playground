66659900024852d6beaf17d74d8d40f1fe4ebd56
Avoracity <avoracity7@gmail.com>
2024-02-28 14:51:13 -0500

    Fade-In-Out component made compatible with react v18

---
+++ src/components/FadeInOut.jsx
@@ -0,0 +1,120 @@
+/* -------------- *\
+  Outsourced from: https://codesandbox.io/p/sandbox/react-fadein-out-transition-component-eww6j?file=%2Fsrc%2FFadeInOut.js%3A3%2C2-105%2C1
+  Defined Component in compliance with React 18 for compatible use
+  Defined Component's Prop Type in compliance with React 18 for compatible use
+
+\* -------------- */
+import React from "react";
+import PropTypes from 'prop-types'; // Import prop-types package
+
+const UNMOUNTED = "unmounted";
+const EXITED = "exited";
+const ENTERING = "entering";
+const ENTERED = "entered";
+const EXITING = "exiting";
+
+const transitionStyles = {
+  entering: { opacity: 0 },
+  entered: { opacity: 1 },
+  exiting: { opacity: 0 },
+  exited: { opacity: 0 }
+};
+
+// Define the Component as a React Component
+class FadeInOut extends React.Component {
+  // Define the Prop Types of the Component
+  static propTypes = {
+    children: PropTypes.node.isRequired,
+    duration: PropTypes.number.isRequired,
+    className: PropTypes.string,
+    style: PropTypes.object,
+    show: PropTypes.object
+  };
+
+  constructor(props) {
+    super(props);
+    this.state = { status: UNMOUNTED };
+  }
+
+  componentDidMount() {
+    const { show } = this.props;
+    if (show) {
+      this.performEnter();
+    }
+  }
+
+  componentDidUpdate(prevProps) {
+    let nextStatus = null;
+    if (prevProps !== this.props) {
+      const { status } = this.state;
+      if (this.props.show) {
+        if (status !== ENTERING && status !== ENTERED) {
+          nextStatus = ENTERING;
+        }
+      } else {
+        if (status === ENTERING || status === ENTERED) {
+          nextStatus = EXITING;
+        }
+      }
+    }
+    this.updateStatus(nextStatus);
+  }
+
+  updateStatus(nextStatus) {
+    if (nextStatus !== null) {
+      if (nextStatus === ENTERING) {
+        this.performEnter();
+      } else {
+        this.performExit();
+      }
+    } else if (this.state.status === EXITED) {
+      this.setState({ status: UNMOUNTED });
+    }
+  }
+
+  performEnter() {
+    this.setState({ status: ENTERING }, () => {
+      setTimeout(() => {
+        this.setState({ status: ENTERED }, () => {});
+      }, 0);
+    });
+  }
+
+  performExit() {
+    const { duration } = this.props;
+    this.setState({ status: EXITING }, () => {
+      setTimeout(() => {
+        this.setState({ status: EXITED }, () => {});
+      }, duration);
+    });
+  }
+
+  render() {
+    const { status } = this.state;
+    if (status === UNMOUNTED) {
+      return null;
+    }
+
+    const { children, duration, className, style } = this.props;
+    return (
+      <div
+        className={className}
+        style={{
+          ...style,
+          transition: `opacity ${duration}ms ease-in-out`,
+          opacity: 0.1,
+          ...transitionStyles[status]
+        }}
+      >
+        {children}
+      </div>
+    );
+  }
+}
+
+FadeInOut.defaultProps = {
+  show: false,
+  duration: 300
+};
+
+export default FadeInOut;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,15 @@
 import "../styles/Home.css";
+import FadeInOut from '../components/FadeInOut.jsx'; // Assuming the 'fadeinout.js' file is in the same directory as 'home.jsx'
 
 export default function Homepage() {
     return(
         <div>
-          <h1 className="text-sky-400 p-8">Welcome to the Homepage</h1>
-            <p>This is the content of the homepage.</p>
+            <FadeInOut show={true} duration={1000}>
+                <h1 className="text-sky-400 p-8">Welcome to the Homepage</h1>
+            </FadeInOut>
+            <FadeInOut show={true} duration={1000}>
+                <p>This is the content of the homepage.</p>
+            </FadeInOut>
         </div>
     );
 }
-
