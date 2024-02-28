c36f1d614b59d527c581d59d0360aa8ea32e2d28
Avoracity <avoracity7@gmail.com>
2024-02-27 22:47:27 -0500

    postcss autoprefixer added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,9 +1,9 @@
-// Homepage.jsx
+import "../styles/Home.css";
 
 export default function Homepage() {
     return(
         <div>
-          <h1>Welcome to the Homepage</h1>
+          <h1 className="text-sky-400 p-8">Welcome to the Homepage</h1>
             <p>This is the content of the homepage.</p>
         </div>
     );
