a72911b333e5ab537b614016ddff9aa07ef2e6f2
Avoracity <avoracity7@gmail.com>
2024-03-02 17:06:38 -0500

    rotate on click

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -1,14 +1,28 @@
 import "../styles/Likes.css";
 import LikesIcon from "../assets/like-icon.svg";
-
+import { useState } from 'react';
 export default function Likes() {
+  const [isClicked, setIsClicked] = useState(false);
+
+  const handleClick = () => {
+    if (!isClicked) {
+      setIsClicked(true);
+      // Rotate the icon back to its original position after 0.5s
+      setTimeout(() => {
+        setIsClicked(false);
+      }, 500);
+    }
+  };
+
   return (
     <>
-    
-      <img src={LikesIcon} alt="Likes Icon" className="like-button w-32 h-32"  /> {/* Render the SVG using an <img> tag */}
-
+      <img
+        src={LikesIcon}
+        alt="Likes Icon"
+        
+        className={`like-button w-32 h-32 ${isClicked ? 'clicked' : ''}`} //*if isClicked true, return clicked, else nothing */
+        onClick={handleClick}
+      />
     </>
   );
 }
-
-
