eebac7964d047c2a00b848a557ebac48422ff593
Avoracity <avoracity7@gmail.com>
2024-03-20 11:58:02 -0400

    body color change via useEffect mount

--- src/pages/Likes.jsx
+++ src/pages/Likes.jsx
@@ -1,8 +1,19 @@
 import "../styles/Likes.css";
 import LikesIcon from "../assets/like-icon.svg";
-import { useState } from 'react';
+import { useState, useEffect  } from 'react';
+
 export default function Likes() {
   const [isClicked, setIsClicked] = useState(false);
+  
+  useEffect(() => {
+    // Add the Tailwind CSS class to the body when the component mounts
+    document.body.classList.add('bg-blue-600');
+
+    // Cleanup function to remove the class when the component unmounts
+    return () => {
+      document.body.classList.remove('bg-blue-600');
+    };
+  }, []); // Empty dependency array ensures the effect runs only once when the component mounts
 
   const handleClick = () => {
     if (!isClicked) {
@@ -11,11 +22,27 @@ export default function Likes() {
       setTimeout(() => {
         setIsClicked(false);
       }, 500);
+
+// top-right line 
+// then bot-left line
+// then top-right firework
+// then bot-right firework
+// then top-left firework
+// then bot-left firework
+
+/*
+      setTimeout(()=>{
+        
+      }, 1000);
+*/
+
+
     }
   };
 
   return (
     <>
+ 
       <img
         src={LikesIcon}
         alt="Likes Icon"
